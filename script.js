a871ba12d472084e253d2fc23fc9ac5a49a6f13d
BeastMaster 64 <244566067+beastmaster64-dev@users.noreply.github.com>
2026-01-03 16:31:26 +0800

    build basic calculator functions

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function addNums(num1, num2){
+    return num1 + num2;
+};
+
+function subtNums(num1,num2){
+    return num1 - num2;
+};
+
+function multiplyNums(num1,num2){
+    return num1 * num2;
+}
+
+function divideNums(num1,num2){
+    if(num2 == 0){
+        return "Can't divide by zero!";
+    }
+    return num1/num2;
+}
+
+
+
